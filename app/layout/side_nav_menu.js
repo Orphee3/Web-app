e2218455c21b115ec553ecc64a8727174e7494aa
superphung <superphung44@gmail.com>
2015-10-10 02:07:58 +0200

    added playlist menu

--- app/layout/side_nav_menu.js
+++ app/layout/side_nav_menu.js
@@ -6,14 +6,20 @@
         .module('orphee-app')
         .controller('SideNavMenuCtrl', SideNavMenuCtrl);
 
-    SideNavMenuCtrl.$inject = ['$auth', 'navservice', '$rootScope'];
+    SideNavMenuCtrl.$inject = ['$q', '$auth', 'navservice', '$rootScope', 'playlistservice'];
 
-    function SideNavMenuCtrl($auth, navservice, $rootScope) {
+    function SideNavMenuCtrl($q, $auth, navservice, $rootScope, playlistservice) {
         var vm = this;
+
+        vm.playlist = [];
 
         vm.isAuthenticated = isAuthenticated;
         vm.isOpen = isOpen;
         vm.toggleFriendNav = toggleFriendNav;
+        vm.togglePlaylistNav = togglePlaylistNav;
+
+        vm.selectPlaylist = selectPlaylist;
+        vm.addPlaylist = addPlaylist;
 
         function isOpen() {
             return navservice.getSideNavState('menu');
@@ -28,6 +34,53 @@
             navservice.setSideNavState('friends');
             console.log(navservice.getSideNavState('friends'));
             $rootScope.$broadcast('refresh nav');
+        }
+
+        function togglePlaylistNav() {
+            console.log('toggle playlist nav');
+            navservice.setSideNavState('playlist');
+        }
+
+        /**
+         *
+         * @param id
+         */
+        function selectPlaylist(id) {
+            playlistservice.setPlaylistActive(id);
+            vm.selectedList = id;
+            $rootScope.$broadcast('refresh playlist');
+        }
+
+        function addPlaylist() {
+            playlistservice.addPlaylist();
+            activate();
+        }
+
+        activate();
+
+        function activate() {
+            var promises = [getPlaylist(), getPlaylistActive()];
+
+            return $q.all(promises)
+                .then(function () {
+                    console.log('activated menu nav');
+                });
+        }
+
+        function getPlaylist() {
+            return playlistservice.getPlaylist()
+                .then(function (data) {
+                    vm.playlist = data;
+                    return vm.playlist;
+                });
+        }
+
+        function getPlaylistActive() {
+            return playlistservice.getPlaylistActive()
+                .then(function (data) {
+                    vm.selectedList = data;
+                    return vm.selectedList;
+                });
         }
     }
 
