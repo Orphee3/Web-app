d7df878994ba96e6f8d26988d2bfa385f850d6d0
superphung <superphung44@gmail.com>
2015-11-03 02:30:20 +0100

    added scrolldown directive for chat

--- app/core/directives.js
+++ app/core/directives.js
@@ -22,5 +22,20 @@
                     scope.$apply();
                 });
             }
+        })
+        .directive('schrollBottom', function () {
+            return {
+                scope: {
+                    schrollBottom: "="
+                },
+                link: function (scope, element) {
+                    scope.$watchCollection('schrollBottom', function (newValue) {
+                        if (newValue)
+                        {
+                            $(element).scrollTop($(element)[0].scrollHeight);
+                        }
+                    });
+                }
+            }
         });
 })();
